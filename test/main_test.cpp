668e167a118226de44b0f616e712e15f444ce6d4
Daulet Tumbayev <daulettbot@gmail.com>
2019-08-28 16:46:33 +0300

    Update main_test.cpp

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -8,37 +8,9 @@
 
 BOOST_AUTO_TEST_SUITE(main_tests)
 
-struct cout_redirect {
-  cout_redirect(std::streambuf* new_buffer)
-    : old(std::cout.rdbuf(new_buffer))
-  { }
-
-  ~cout_redirect() {
-    std::cout.rdbuf(old);
-  }
-
-private:
-  std::streambuf* old;
-};
-
 BOOST_AUTO_TEST_CASE(no_input)
 {
-  boost::test_tools::output_test_stream output;
-  boost::test_tools::output_test_stream check;
-
-  {
-    cout_redirect guard(output.rdbuf());
-
-    BOOST_CHECK_EQUAL(main(1, "CifeProject"), 1);
-  }
-
-  {
-    cout_redirect guard(check.rdbuf());
-
-    helper::show_usage();
-  }
-
-  BOOST_CHECK(output.output_test_stream("Test\n"));
+  
 }
 
 BOOST_AUTO_TEST_CASE(wrong_input)
